9e411069c4bc6e49aac48e5d0568201a4ee16cf5
Bwaaahhh <cy.etievant@gmail.com>
2017-07-01 11:10:59 +0200

    update controller.

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -3,23 +3,43 @@
 namespace OC\PlatformBundle\Controller ;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller ;
+use Symfony\Component\HttpFoundation\Request ;
 use Symfony\Component\HttpFoundation\Response ;
 
 class AdvertController extends Controller
 {
+    public function viewAction($id, Request $request)
+    {
+        $tag = $request->query->get('tag');
+
+        return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+            'id' => $id ,
+            'tag' => $tag,
+        ));
+    }
+
+    public function viewSlugAction($slug , $year , $format)
+    {
+        return new Response(
+            "On pourrait afficher l'annonce correspondant au slug
+             '".$slug."', crée en ".$year." et au format ".$format."."
+        );
+    }
+
     public function indexAction()
     {
-        $content = $this
-            ->get('templating')
-            ->render('OCPlatformBundle:Advert:index.html.twig',array('nom'=>'Bwaaahhh'));
+        $url = $this->get('router')->generate(
+            'oc_platform_view',
+            array('id' => 5 )
+        );
 
-        return new Response($content);
+        return new Response("L'URL de l'annonce d'id 5 est ".$url);
+//        $content = $this
+//            ->get('templating')
+//            ->render('OCPlatformBundle:Advert:index.html.twig',array('nom'=>'Bwaaahhh'));
+//
+//        return new Response($content);
     }
 
-    public function byeAction()
-    {
-        $content = $this->get('templating')->render('OCPlatformBundle:Advert:bye.html.twig');
 
-        return new Response($content);
-    }
 }
